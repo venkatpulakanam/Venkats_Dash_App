6197237cb8ff56fb8cda105c8f8efc8f6be51c74
venkatpulakanam <51539031+venkatpulakanam@users.noreply.github.com>
2024-10-27 17:50:09 +1300

    Update Deploying_Dash_App_On_Render_24Oct2024.py

--- Deploying_Dash_App_On_Render_24Oct2024.py
+++ Deploying_Dash_App_On_Render_24Oct2024.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 # Deploying Dash App using Render: An Example
-# Source: https://docs.google.com/document/d/15P9TIIxKHujkesBnYL_4nLU3Pr9AEKw12Ov-X1Bca7s/edit?tab=t.0
+import os
 import numpy as np
 import pandas as pd
 from dash import Dash, dcc, html, Output, Input
 import plotly.express as px
-#
+
+# Load dataset
 df = px.data.tips()
+
+# Initialize Dash app
 app = Dash(__name__)
-server = app.server # added this to use the app via Render
-#
+server = app.server  # required for Render deployment
+
+# Define layout
 radio_b = dcc.RadioItems(df.columns, 'time')
 my_graph = dcc.Graph()
 app.layout = html.Div([radio_b, my_graph])
 
-@app.callback(Output(my_graph, 'figure'),
-              Input(radio_b, 'value'))
+# Define callback
+@app.callback(Output(my_graph, 'figure'), Input(radio_b, 'value'))
 def update_graph(value):
-   fig = px.bar(df, x=value, y="tip")
-   return fig
+    fig = px.bar(df, x=value, y="tip")
+    return fig
+
+# Run app on Render-compatible host and port
 if __name__ == '__main__':
-  app.run()
-# if __name__ == '__main__':
-  #  app.run_server(debug=False)
-
-
-# In[ ]:
-
-
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
